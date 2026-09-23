b62829ca4ee7e341f04aa34f1b3f9c5f17b992fd
salguento <salguento@gmail.com>
2026-09-22 22:33:55 -0300

    test(core): limit concurrent audio fixtures

--- durvald-core/tests/common/core.rs
+++ durvald-core/tests/common/core.rs
@@ -3,12 +3,87 @@ use durvald_core::CoreConfig;
 use std::error::Error;
 
 #[cfg(feature = "test-support")]
-use std::sync::Arc;
+use std::sync::{Arc, OnceLock};
+
+#[cfg(feature = "test-support")]
+use tokio::sync::{OwnedSemaphorePermit, Semaphore};
 
 #[cfg(feature = "test-support")]
 use durvald_core::DurvaldCore;
 
 use super::filesystem::TestFs;
+
+#[cfg(feature = "test-support")]
+static TEST_CORE_LIMIT: OnceLock<Arc<Semaphore>> = OnceLock::new();
+
+#[cfg(feature = "test-support")]
+fn test_core_limit() -> Arc<Semaphore> {
+    TEST_CORE_LIMIT
+        .get_or_init(|| Arc::new(Semaphore::new(4)))
+        .clone()
+}
+
+#[cfg(feature = "test-support")]
+pub struct TestCore {
+    core: Arc<DurvaldCore>,
+    files: TestFs,
+    _permit: OwnedSemaphorePermit,
+}
+
+#[cfg(feature = "test-support")]
+impl TestCore {
+    pub async fn open() -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let files = TestFs::new()?;
+
+        Self::open_with_files(files).await
+    }
+
+    pub async fn open_with_files(files: TestFs) -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let permit = test_core_limit().acquire_owned().await?;
+
+        let config = files.core_config();
+
+        let core = durvald_core::test_support::open_core(config).await?;
+
+        Ok(Self {
+            core,
+            files,
+            _permit: permit,
+        })
+    }
+
+    pub fn core(&self) -> &Arc<DurvaldCore> {
+        &self.core
+    }
+
+    pub fn files(&self) -> &TestFs {
+        &self.files
+    }
+
+    pub async fn restart(self) -> Result<Self, Box<dyn Error + Send + Sync>> {
+        let Self {
+            core,
+            files,
+            _permit,
+        } = self;
+
+        drop(core);
+
+        let config = files.core_config();
+
+        let core = durvald_core::test_support::open_core(config).await?;
+
+        Ok(Self {
+            core,
+            files,
+            _permit,
+        })
+    }
+
+    pub async fn process_mock_audio(&self, blocks: usize) {
+        durvald_core::test_support::process_mock_audio(self.core.as_ref(), blocks).await;
+    }
+}
 
 impl TestFs {
     pub fn core_config(&self) -> CoreConfig {
@@ -28,51 +103,5 @@ impl TestFs {
             .unwrap_or("unknown");
 
         format!("durvald-test-{directory_name}")
-    }
-}
-
-#[cfg(feature = "test-support")]
-pub struct TestCore {
-    core: Arc<DurvaldCore>,
-    files: TestFs,
-}
-
-#[cfg(feature = "test-support")]
-impl TestCore {
-    pub async fn open() -> Result<Self, Box<dyn Error + Send + Sync>> {
-        let files = TestFs::new()?;
-
-        Self::open_with_files(files).await
-    }
-
-    pub async fn open_with_files(files: TestFs) -> Result<Self, Box<dyn Error + Send + Sync>> {
-        let config = files.core_config();
-
-        let core = durvald_core::test_support::open_core(config).await?;
-
-        Ok(Self { core, files })
-    }
-
-    pub fn core(&self) -> &Arc<DurvaldCore> {
-        &self.core
-    }
-
-    pub fn files(&self) -> &TestFs {
-        &self.files
-    }
-
-    pub async fn restart(self) -> Result<Self, Box<dyn Error + Send + Sync>> {
-        let Self { core, files } = self;
-
-        drop(core);
-
-        let config = files.core_config();
-        let core = durvald_core::test_support::open_core(config).await?;
-
-        Ok(Self { core, files })
-    }
-
-    pub async fn process_mock_audio(&self, blocks: usize) {
-        durvald_core::test_support::process_mock_audio(self.core.as_ref(), blocks).await;
     }
 }
